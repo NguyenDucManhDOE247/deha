9094b98b37f14f5512717ff5258a0887c4619eec
daituonglevi <133839834+daituonglevi@users.noreply.github.com>
2025-05-21 20:09:46 -0700

    Update 0001_initial.py

--- userauth/migrations/0001_initial.py
+++ userauth/migrations/0001_initial.py
@@ -26,7 +26,6 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.UUIDField(default=uuid.uuid4, primary_key=True, serialize=False)),
                 ('user', models.CharField(max_length=100)),
                 ('image', models.ImageField(upload_to='post_images')),
                 ('caption', models.TextField()),
